d4cf91972575abf043445c7a3f5e6e335bf74f08
Corentin Kervadec HPC UPF <corentin.kervadec@wanadoo.fr>
2024-01-22 16:06:22 +0100

    fix bug in print

--- src/prompt/machine_prompt.py
+++ src/prompt/machine_prompt.py
@@ -462,14 +462,15 @@ class RandomPairPromptSearch(DiscreteGradientPromptSearch):
             df_candidates['gt_prob'] = df_candidates.apply(lambda row: row['probs'][self.model.tokenizer.encode(row['label'], add_special_tokens=False)[0]], axis=1)  # get the prob associated to the groundtruth
             population_template = [(d[0], d[2], d[1]) for d in df_candidates.groupby(['template','tid'])['gt_prob'].mean().reset_index().values.tolist()]\
                         + [t for t in template_candidates if t[1] is not None]
+            print(df_candidates[['prompt', 'label', 'pred', 'gt_prob']])
         else:
             df_candidates['correct'] = df_candidates.apply(lambda row: row['label'] in row['pred'], axis=1)  
             population_template = [(d[0], d[2], d[1]) for d in df_candidates.groupby(['template','tid'])['correct'].mean().reset_index().values.tolist()]\
                         + [t for t in template_candidates if t[1] is not None]
+            print(df_candidates[['prompt', 'label', 'pred', 'correct']])
         # todo: use a more balanced metric like below
         # result = df_candidates.groupby(['template','tid','label']).mean('correct').groupby(['template','tid']).mean().reset_index().values.tolist()
 
-        print(df_candidates[['prompt', 'label', 'pred', 'gt_prob']])
         # redupplicate
         population_template = self.reduplicate_templates(population_template, population_template_undup_count)
         
